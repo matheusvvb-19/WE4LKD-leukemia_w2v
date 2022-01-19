99a8f416f26c9a70bd43e8e29ae05be2db3bffa2
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2022-01-19 09:12:57 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -233,7 +233,9 @@ if uploaded_file is not None:
     model = pickle.load(uploaded_file)
     #model = KeyedVectors.load_word2vec_format(open(uploaded_file))
     model.init_sims()
-    common_words = get_most_common(5000)
+    common_words_number = st.sidebar.selectbox('Select the amount of english common words that you want to remove from visualization ',
+    ('5000', '10000', '15000', '20000'))
+    common_words = get_most_common(int(common_words_number))
     restrict_w2v(model, set(common_words))
 
 dim_red = st.sidebar.selectbox(
