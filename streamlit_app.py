027b08dc472f020cf3ee947b5b7da4e660c98496
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2022-01-19 16:07:34 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,30 @@ def restrict_w2v(w2v, restricted_word_set):
     w2v.index2entity = np.array(new_index2entity)
     w2v.index2word = np.array(new_index2entity)
     w2v.vectors_norm = np.array(new_vectors_norm)
+    
+def wv_restrict_w2v(w2v, restricted_word_set):
+    new_vectors = []
+    new_vocab = {}
+    new_index2entity = []
+    new_vectors_norm = []
+
+    for i in range(len(w2v.wv.vocab)):
+        word = w2v.wv.index2entity[i]
+        vec = w2v.wv.vectors[i]
+        vocab = w2v.wv.vocab[word]
+        vec_norm = w2v.wv.vectors_norm[i]
+        if word not in restricted_word_set:
+            vocab.index = len(new_index2entity)
+            new_index2entity.append(word)
+            new_vocab[word] = vocab
+            new_vectors.append(vec)
+            new_vectors_norm.append(vec_norm)
+
+    w2v.wv.vocab = new_vocab
+    w2v.wv.vectors = np.array(new_vectors)
+    w2v.wv.index2entity = np.array(new_index2entity)
+    w2v.wv.index2word = np.array(new_index2entity)
+    w2v.wv.vectors_norm = np.array(new_vectors_norm)
 
 def append_list(sim_words, words):
     list_of_words = []
