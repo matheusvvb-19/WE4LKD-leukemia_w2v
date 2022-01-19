52abee78b1dfcdcb2cae2527c21df0f8531648fc
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2022-01-18 22:49:42 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,23 +16,23 @@ def restrict_w2v(w2v, restricted_word_set):
     new_index2entity = []
     new_vectors_norm = []
 
-    for i in range(len(w2v.wv.vocab)):
-        word = w2v.wv.index2entity[i]
-        vec = w2v.wv.vectors[i]
-        vocab = w2v.wv.vocab[word]
-        vec_norm = w2v.wv.vectors_norm[i]
-        if word not in restricted_word_set:
+    for i in range(len(w2v.vocab)):
+        word = w2v.index2entity[i]
+        vec = w2v.vectors[i]
+        vocab = w2v.vocab[word]
+        vec_norm = w2v.vectors_norm[i]
+        if word in restricted_word_set:
             vocab.index = len(new_index2entity)
             new_index2entity.append(word)
             new_vocab[word] = vocab
             new_vectors.append(vec)
             new_vectors_norm.append(vec_norm)
 
-    w2v.wv.vocab = new_vocab
-    w2v.wv.vectors = new_vectors
-    w2v.wv.index2entity = new_index2entity
-    w2v.wv.index2word = new_index2entity
-    w2v.wv.vectors_norm = new_vectors_norm
+    w2v.vocab = new_vocab
+    w2v.vectors = np.array(new_vectors)
+    w2v.index2entity = np.array(new_index2entity)
+    w2v.index2word = np.array(new_index2entity)
+    w2v.vectors_norm = np.array(new_vectors_norm)
 
 def append_list(sim_words, words):
     list_of_words = []
