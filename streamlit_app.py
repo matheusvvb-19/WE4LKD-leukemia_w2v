241311759be27aa2c7c536f2b175edd4d155d14d
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2022-01-23 13:13:16 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,13 +73,10 @@ def append_list(sim_words, words):
 
 def display_scatterplot_3D(model, user_input=None, words=None, label=None, color_map=None, annotation='On',  dim_red = 'TSNE', perplexity = 0, learning_rate = 0, iteration = 0, topn=0, sample=10):
     if words == None:
-        """
         if sample > 0:
             words = np.random.choice(list(model.wv.vocab.keys()), sample)
         else:
             words = [word for word in model.wv.vocab]
-        """
-        words = [word for word in model.wv.vocab]
     
     word_vectors = np.array([model[w] for w in words])
     
@@ -177,13 +174,10 @@ def horizontal_bar(word, similarity):
 
 def display_scatterplot_2D(model, user_input=None, words=None, label=None, color_map=None, annotation='On', dim_red = 'TSNE', perplexity = 0, learning_rate = 0, iteration = 0, topn=0, sample=10):
     if words == None:
-        """
         if sample > 0:
             words = np.random.choice(list(model.wv.vocab.keys()), sample)
         else:
             words = [word for word in model.wv.vocab]
-        """
-        words = [word for word in model.wv.vocab]
     
     word_vectors = np.array([model[w] for w in words])
     
