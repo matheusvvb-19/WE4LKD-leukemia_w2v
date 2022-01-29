0cee8fa18abf085462216718722e925bd4ae7e51
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2022-01-29 16:56:05 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,23 +20,36 @@ domains_table = pd.read_csv('https://docs.google.com/spreadsheets/d/' +
                    '/export?gid=0&format=csv',
                   )
 
-def restrict_w2v(w2v, restricted_word_set):
+def restrict_w2v(w2v, restricted_word_set, domain=False):
     new_vectors = []
     new_vocab = {}
     new_index2entity = []
     new_vectors_norm = []
 
-    for i in range(len(w2v.vocab)):
-        word = w2v.index2entity[i]
-        vec = w2v.vectors[i]
-        vocab = w2v.vocab[word]
-        vec_norm = w2v.vectors_norm[i]
-        if word not in restricted_word_set:
-            vocab.index = len(new_index2entity)
-            new_index2entity.append(word)
-            new_vocab[word] = vocab
-            new_vectors.append(vec)
-            new_vectors_norm.append(vec_norm)
+    if domain == False:
+      for i in range(len(w2v.vocab)):
+          word = w2v.index2entity[i]
+          vec = w2v.vectors[i]
+          vocab = w2v.vocab[word]
+          vec_norm = w2v.vectors_norm[i]
+          if word not in restricted_word_set:
+              vocab.index = len(new_index2entity)
+              new_index2entity.append(word)
+              new_vocab[word] = vocab
+              new_vectors.append(vec)
+              new_vectors_norm.append(vec_norm)
+    else:
+      for i in range(len(w2v.vocab)):
+          word = w2v.index2entity[i]
+          vec = w2v.vectors[i]
+          vocab = w2v.vocab[word]
+          vec_norm = w2v.vectors_norm[i]
+          if word in restricted_word_set:
+              vocab.index = len(new_index2entity)
+              new_index2entity.append(word)
+              new_vocab[word] = vocab
+              new_vectors.append(vec)
+              new_vectors_norm.append(vec_norm)
 
     w2v.vocab = new_vocab
     w2v.vectors = np.array(new_vectors)
@@ -44,47 +57,36 @@ def restrict_w2v(w2v, restricted_word_set):
     w2v.index2word = np.array(new_index2entity)
     w2v.vectors_norm = np.array(new_vectors_norm)
     
-def wv_restrict_w2v(w2v, restricted_word_set):
+def wv_restrict_w2v(w2v, restricted_word_set, domain=False):
     new_vectors = []
     new_vocab = {}
     new_index2entity = []
     new_vectors_norm = []
 
-    for i in range(len(w2v.wv.vocab)):
-        word = w2v.wv.index2entity[i]
-        vec = w2v.wv.vectors[i]
-        vocab = w2v.wv.vocab[word]
-        vec_norm = w2v.wv.vectors_norm[i]
-        if word not in restricted_word_set:
-            vocab.index = len(new_index2entity)
-            new_index2entity.append(word)
-            new_vocab[word] = vocab
-            new_vectors.append(vec)
-            new_vectors_norm.append(vec_norm)
-
-    w2v.wv.vocab = new_vocab
-    w2v.wv.vectors = np.array(new_vectors)
-    w2v.wv.index2entity = np.array(new_index2entity)
-    w2v.wv.index2word = np.array(new_index2entity)
-    w2v.wv.vectors_norm = np.array(new_vectors_norm)
-    
-def domain_w2v(w2v, restricted_word_set):
-    new_vectors = []
-    new_vocab = {}
-    new_index2entity = []
-    new_vectors_norm = []
-
-    for i in range(len(w2v.wv.vocab)):
-        word = w2v.wv.index2entity[i]
-        vec = w2v.wv.vectors[i]
-        vocab = w2v.wv.vocab[word]
-        vec_norm = w2v.wv.vectors_norm[i]
-        if word in restricted_word_set:
-            vocab.index = len(new_index2entity)
-            new_index2entity.append(word)
-            new_vocab[word] = vocab
-            new_vectors.append(vec)
-            new_vectors_norm.append(vec_norm)
+    if domain == False:
+      for i in range(len(w2v.wv.vocab)):
+          word = w2v.wv.index2entity[i]
+          vec = w2v.wv.vectors[i]
+          vocab = w2v.wv.vocab[word]
+          vec_norm = w2v.wv.vectors_norm[i]
+          if word not in restricted_word_set:
+              vocab.index = len(new_index2entity)
+              new_index2entity.append(word)
+              new_vocab[word] = vocab
+              new_vectors.append(vec)
+              new_vectors_norm.append(vec_norm)
+    else:
+      for i in range(len(w2v.vocab)):
+          word = w2v.index2entity[i]
+          vec = w2v.vectors[i]
+          vocab = w2v.vocab[word]
+          vec_norm = w2v.vectors_norm[i]
+          if word in restricted_word_set:
+              vocab.index = len(new_index2entity)
+              new_index2entity.append(word)
+              new_vocab[word] = vocab
+              new_vectors.append(vec)
+              new_vectors_norm.append(vec_norm)
 
     w2v.wv.vocab = new_vocab
     w2v.wv.vectors = np.array(new_vectors)
@@ -107,7 +109,7 @@ def display_scatterplot_3D(model, user_input=None, words=None, label=None, color
     if restrict != 'geral':
         if restrict == 'câncer':
             specific_domain = list(dict.fromkeys(domains_table['name']))
-            domain_w2v(model, specific_domain)
+            wv_restrict_w2v(model, specific_domain, True)
     
     if words == None:
         if sample > 0:
@@ -213,7 +215,7 @@ def display_scatterplot_2D(model, user_input=None, words=None, label=None, color
     if restrict != 'geral':
         if restrict == 'câncer':
             specific_domain = list(dict.fromkeys(domains_table['name']))
-            domain_w2v(model, specific_domain)
+            wv_restrict_w2v(model, specific_domain, True)
     
     if words == None:
         if sample > 0:
