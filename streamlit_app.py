767b1419f71a933b00707c513ddc85dbf6606c62
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2022-01-31 14:22:47 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -114,76 +114,77 @@ def display_scatterplot_3D(model, user_input=None, words=None, label=None, color
     
     word_vectors = np.array([model[w] for w in words])
     
-    if dim_red == 'PCA':
-        three_dim = PCA(random_state=0).fit_transform(word_vectors)[:,:3]
-    else:
-        three_dim = TSNE(n_components = 3, random_state=0, perplexity = perplexity, learning_rate = learning_rate, n_iter = iteration).fit_transform(word_vectors)[:,:3]
+    if len(word_vector) > 0:
+        if dim_red == 'PCA':
+            three_dim = PCA(random_state=0).fit_transform(word_vectors)[:,:3]
+        else:
+            three_dim = TSNE(n_components = 3, random_state=0, perplexity = perplexity, learning_rate = learning_rate, n_iter = iteration).fit_transform(word_vectors)[:,:3]
 
-    data = []
-    count = 0
-    for i in range (len(user_input)):
+        data = []
+        count = 0
+        for i in range (len(user_input)):
 
-                trace = go.Scatter3d(
-                    x = three_dim[count:count+topn,0], 
-                    y = three_dim[count:count+topn,1],  
-                    z = three_dim[count:count+topn,2],
-                    text = words[count:count+topn] if annotation == 'On' else '',
-                    name = user_input[i],
-                    textposition = "top center",
-                    textfont_size = 30,
-                    mode = 'markers+text',
-                    marker = {
-                        'size': 10,
-                        'opacity': 0.8,
-                        'color': 2
-                    }
-                )
-               
-                data.append(trace)
-                count = count+topn
-
-    trace_input = go.Scatter3d(
-                    x = three_dim[count:,0], 
-                    y = three_dim[count:,1],  
-                    z = three_dim[count:,2],
-                    text = words[count:],
-                    name = 'input words',
-                    textposition = "top center",
-                    textfont_size = 30,
-                    mode = 'markers+text',
-                    marker = {
-                        'size': 10,
-                        'opacity': 1,
-                        'color': 'black'
-                    }
+                    trace = go.Scatter3d(
+                        x = three_dim[count:count+topn,0], 
+                        y = three_dim[count:count+topn,1],  
+                        z = three_dim[count:count+topn,2],
+                        text = words[count:count+topn] if annotation == 'On' else '',
+                        name = user_input[i],
+                        textposition = "top center",
+                        textfont_size = 30,
+                        mode = 'markers+text',
+                        marker = {
+                            'size': 10,
+                            'opacity': 0.8,
+                            'color': 2
+                        }
                     )
 
-    data.append(trace_input)
+                    data.append(trace)
+                    count = count+topn
+
+        trace_input = go.Scatter3d(
+                        x = three_dim[count:,0], 
+                        y = three_dim[count:,1],  
+                        z = three_dim[count:,2],
+                        text = words[count:],
+                        name = 'input words',
+                        textposition = "top center",
+                        textfont_size = 30,
+                        mode = 'markers+text',
+                        marker = {
+                            'size': 10,
+                            'opacity': 1,
+                            'color': 'black'
+                        }
+                        )
+
+        data.append(trace_input)
     
-# Configure the layout.
-    layout = go.Layout(
-        margin = {'l': 0, 'r': 0, 'b': 0, 't': 0},
-        showlegend=True,
-        legend=dict(
-        x=1,
-        y=0.5,
-        font=dict(
-            family="Courier New",
-            size=25,
-            color="black"
-        )),
-        font = dict(
-            family = " Courier New ",
-            size = 15),
-        autosize = False,
-        width = 1000,
-        height = 1000
-        )
+    # Configure the layout.
+        layout = go.Layout(
+            margin = {'l': 0, 'r': 0, 'b': 0, 't': 0},
+            showlegend=True,
+            legend=dict(
+            x=1,
+            y=0.5,
+            font=dict(
+                family="Courier New",
+                size=25,
+                color="black"
+            )),
+            font = dict(
+                family = " Courier New ",
+                size = 15),
+            autosize = False,
+            width = 1000,
+            height = 1000
+            )
 
 
-    plot_figure = go.Figure(data = data, layout = layout)
-    plot_figure.update_layout(scene=dict(xaxis_showspikes=False, yaxis_showspikes=False, zaxis_showspikes=False))
-    st.plotly_chart(plot_figure)
+        plot_figure = go.Figure(data = data, layout = layout)
+        plot_figure.update_layout(scene=dict(xaxis_showspikes=False, yaxis_showspikes=False, zaxis_showspikes=False))
+        st.plotly_chart(plot_figure)
 
 def horizontal_bar(word, similarity):
     similarity = [round(elem, 2) for elem in similarity]
