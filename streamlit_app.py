aefc80fe71841cc3692fc711e575e5507221dfc7
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2022-02-14 20:53:05 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -496,26 +496,24 @@ if user_input != '':
         st.write('NUMBER CONTAINERS: {}'.format(number_containers))
         subplots_plots_div = st.container()
         with subplots_plots_div:
-            for w in user_input:
-                if number_containers > 0:
-                    subplots_plots_div_row = st.container()
-                    with subplots_plots_div_row:
-                        col1, col2 = subplots_plots_div_row.columns(2)
-
-                        if i % 2 == 0:
-                            with col1:
-                                col1_plot = col1.empty()
-                                with col1_plot:
-                                    horizontal_bar(similar_word[count:count+top_n], similarity[count:count+top_n], w)
-                        else:
-                            with col2:
-                                col2_plot = col2.empty()
-                                with col2_plot:
-                                    horizontal_bar(similar_word[count:count+top_n], similarity[count:count+top_n], w)
-                        count = count + top_n
-                        i = i + 1
-
-                    number_containers -= 1                
+            for j in range(number_containers):
+                subplots_plots_div_row = subplots_plots_div.container()
+                col1, col2 = subplots_plots_div_row.columns(2)
+                col1_plot = col1.empty()
+                col2_plot = col2.empty()
+                
+                with col1_plot:
+                    horizontal_bar(similar_word[count:count+top_n], similarity[count:count+top_n], user_input[i])
+                
+                i = i + 1 
+                try:
+                    with col2_plot:
+                        horizontal_bar(similar_word[count:count+top_n], similarity[count:count+top_n], user_input[i])
+                except:
+                    pass
+                    
+                count = count + top_n
+                i = i + 1     
             
     form_section = st.container()
     new_words_to_search = []
