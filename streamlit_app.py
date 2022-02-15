4ebe8c7b6d5303857183bd7fa2360ebea1afc54c
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2022-02-14 22:04:14 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -526,7 +526,7 @@ if user_input != '':
         
         form_selection_div = st.empty()
         with form_selection_div:
-            form = form_selection_div.form(key='similar_words_form')
+            form = form_selection_div.form(key='similar_words_form', clear_on_submit=True)
             with form:
                 cols = st.columns(number_terms)
                 for k, col in enumerate(cols):
@@ -613,7 +613,7 @@ if user_input != '':
             new_words_to_search = []
             form_selection_div = st.empty()
             with form_selection_div:
-                form = form_selection_div.form(key='similar_words_form2')
+                form = form_selection_div.form(key='similar_words_form2', clear_on_submit=True)
                 with form:
                     cols = st.columns(number_terms)
                     for k, col in enumerate(cols):
