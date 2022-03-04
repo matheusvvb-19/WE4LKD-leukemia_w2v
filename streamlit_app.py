59e9438e10297d6ca9cbde17c953e61898d12ac3
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2022-03-04 16:23:09 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -559,7 +559,7 @@ if user_input != '':
                 with form:
                     cols = st.columns(number_terms)
                     for k, col in enumerate(cols):
-                        selected_words = col.multiselect(user_input[k], options_list[k], key=k)
+                        selected_words = col.multiselect(user_input[k], options_list[k])
                         new_words_to_search.extend(selected_words)
 
                     new_words_to_search = list(dict.fromkeys(new_words_to_search))
