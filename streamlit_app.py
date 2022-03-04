5b923f25aaded2c57283089780d3d2cd540a621c
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2022-03-04 16:06:47 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -555,7 +555,7 @@ if user_input != '':
         form_selection_div = st.empty()
         with form_selection_div:
             form = form_selection_div.form(key='similar_words_form')
-            while(true):
+            while(True):
                 with form:
                     cols = st.columns(number_terms)
                     for k, col in enumerate(cols):
