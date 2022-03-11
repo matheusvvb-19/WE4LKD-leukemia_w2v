c19932dea0d51942a5ac15560bc34805fd683a98
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2022-03-11 15:04:40 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -353,26 +353,25 @@ def set_page_layout():
                 position: relative;
                 padding: 5px;
                 top: 2px;
+                color: black;
             }
             </style>
             """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-    
+
+
     st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
     st.markdown("""
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #8C8988;">
-      <a class="navbar-brand" href="https://share.streamlit.io/matheusvvb-19/we4lkd-leukemia_w2v/main" target="_blank">Embedding Viewer</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
+        <a class="navbar-brand" href="#" target="_blank">Embedding Viewer</a>
+        <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
-          <li class="nav-item active">
+            <li class="nav-item active">
             <a class="nav-link disabled" href="https://github.com/matheusvvb-19/WE4LKD-leukemia_w2v" target="_blank">GitHub Repository <span class="sr-only">(current)</span></a>
-          </li>
+            </li>
         </ul>
-      </div>
+        </div>
     </nav>
     """, unsafe_allow_html=True)
 
