33669a60c3f9ee34c428697a6d95782f8e38e802
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2022-03-11 18:00:53 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -338,6 +338,11 @@ def set_page_layout():
         page_icon="🖥️",
         layout="wide",
         initial_sidebar_state="expanded"
+        menu_items={
+            'Get Help': 'https://www.extremelycoolapp.com/help',
+            'Report a bug': "https://www.extremelycoolapp.com/bug",
+            'About': "# This is a header. This is an *extremely* cool app!"
+        }
      )
     
     hide_streamlit_style = """
