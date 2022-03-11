cb5d43b0062c720a1411ee938c2fc0730266ceb4
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2022-03-11 14:41:24 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -357,6 +357,29 @@ def set_page_layout():
             </style>
             """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+    st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
+    st.markdown("""
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
+      <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Data Professor</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://youtube.com/dataprofessor" target="_blank">YouTube</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://twitter.com/thedataprof" target="_blank">Twitter</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    """, unsafe_allow_html=True)
 
 def plot_data_config(user_input, model):
     result_word = []
