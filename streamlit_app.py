da5979dd654e5f6418a8bccd61bf9d1ec5946dfa
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2022-03-11 14:42:07 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -357,6 +357,7 @@ def set_page_layout():
             </style>
             """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+    '''
     st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
     st.markdown("""
@@ -380,6 +381,7 @@ def set_page_layout():
       </div>
     </nav>
     """, unsafe_allow_html=True)
+    '''
 
 def plot_data_config(user_input, model):
     result_word = []
