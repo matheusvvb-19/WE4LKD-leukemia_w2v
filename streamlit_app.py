712c43d9da44e427f2c82511f9c85eafde99464d
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2022-03-13 17:52:33 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -423,8 +423,8 @@ def deep_search(user_input, new_words_to_search, plot_container, dimension, mode
         else:
             display_scatterplot_3D(model, user_input, similar_word, labels, color_map, annotation, dim_red, perplexity, learning_rate, iteration, top_n)
 
+    table_cells_div.empty()
     with table_section:
-        table_cells_div.empty()
         table_cells_div = st.empty()
         with table_cells_div:
             similarities_table_streamlit(user_input, model)
