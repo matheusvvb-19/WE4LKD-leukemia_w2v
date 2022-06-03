cfa0b63ebe1b4f2719f1760e99068f2e53649e61
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2022-06-03 17:15:10 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ def process_entity_list(entity_list):
 def create_entities_lists():
     url = 'https://drive.google.com/file/d/1Q-lA9xtZztUETz5zJrbdN0Fdpg96u2y7/view?usp=sharing'
     path = 'https://drive.google.com/uc?id=' + '1Q-lA9xtZztUETz5zJrbdN0Fdpg96u2y7'
-    entities_table = pd.read_csv(path)
+    entities_table = pd.read_csv(path, sep=',', escapechar='\\')
     
     list_diseases = entities_table[entities_table['entities'].str.contains('oncological|disease_syndrome_disorder|symptom|treatment|cancer')]['word'].to_list()
     list_drugs_chemicals = entities_table[entities_table['entities'].str.contains('substance|drug_ingredient|drug_brandname|drugchem|drug')]['word'].to_list()
