9b7bebc3b490aea6c3a5a7f89bc851dbe5aaf5ce
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2023-01-03 16:32:42 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -41,3 +41,19 @@ if __name__ == '__main__':
     set_page_layout()
     
     st.title('Word Embedding For Latent Knowledge Discovery AML')
+    
+    st.header('1. Introduction')
+    st.text('lalala')
+    
+    st.header('2. Approaches')
+    st.subheader('2.1 Word2Vec')
+    st.text('lalala')
+    
+    st.subheader('2.2 BERT-based')
+    st.text('lalala')
+    
+    st.header('3. About this web app')
+    st.text('lalala')
+    
+    st.header('References')
+    st.text('[1] Vahe Tshitoyan et al. “Unsupervised word embeddings capture latent knowledge frommaterials science literature”. Nature571 (2019), pp. 95–98')
