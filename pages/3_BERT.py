6607a08a40fdce71a37e3a57dfe5ea2a2e6d4dc5
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2023-01-04 13:20:11 -0300

    Update 3_BERT.py

--- pages/3_BERT.py
+++ pages/3_BERT.py
@@ -24,5 +24,15 @@ if __name__ == '__main__':
             """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
     
+    with st.sidebar.form('sidebar_form'):
+        st.header('Models exploration settings')
+        
+        loaded_model = st.selectbox(
+         'Choose one of the preloaded models:',
+         ('10: 1900 - 2022', '9: 1900 - 2016', '8: 1900 - 2014', '7: 1900 - 2013', '6: 1900 - 2011', '5: 1900 - 2009', '4: 1900 - 2001', '3: 1900 - 1999', '2: 1900 - 1977', '1: 1900 - 1967'))
+        
+        top_n = st.slider('Select the neighborhood size',
+            5, 20, (5), 5)
+        
     st.sidebar.header('GitHub Repository')
     st.sidebar.markdown("[![Foo](https://cdn-icons-png.flaticon.com/32/25/25231.png)](https://github.com/matheusvvb-19/WE4LKD-leukemia_w2v)")
