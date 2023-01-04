d0c1ab8a518303eb45e3661750fc4eebb4fd78a5
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2023-01-04 16:02:47 -0300

    Update 3_BERT.py

--- pages/3_BERT.py
+++ pages/3_BERT.py
@@ -82,47 +82,48 @@ if __name__ == '__main__':
         input_sentence = st.text_input(label='Input sentence', max_chars=128, help='Type the sentence that you want to compare to the others.')
         
         submitted = st.form_submit_button('Apply settings')
-        if submitted or st.session_state['execution_counter'] != 0:
-            st.session_state['execution_counter'] += 1
-            
-            sentences.insert(0, input_sentence)
-            
-            tokenizer = AutoTokenizer.from_pretrained('microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext')
-            model = AutoModel.from_pretrained('matheusvolpon/WE4LKD_AML_distilbert_1921_{}'.format(loaded_model[-4:]))
-            
-            # initialize dictionary that will contain tokenized sentences
-            tokens = {'input_ids': [], 'attention_mask': []}
+        
+    if submitted or st.session_state['execution_counter'] != 0:
+        st.session_state['execution_counter'] += 1
 
-            for sentence in sentences:
-                # tokenize sentence and append to dictionary lists
-                new_tokens = tokenizer.encode_plus(sentence, max_length=128, truncation=True,
-                                                   padding='max_length', return_tensors='pt')
-                tokens['input_ids'].append(new_tokens['input_ids'][0])
-                tokens['attention_mask'].append(new_tokens['attention_mask'][0])
+        sentences.insert(0, input_sentence)
 
-            # reformat list of tensors into single tensor
-            tokens['input_ids'] = torch.stack(tokens['input_ids'])
-            tokens['attention_mask'] = torch.stack(tokens['attention_mask'])
-            
-            outputs = model(**tokens)
-            
-            embeddings = outputs.last_hidden_state
-            
-            attention_mask = tokens['attention_mask']
-            
-            mask = attention_mask.unsqueeze(-1).expand(embeddings.size()).float()
-            
-            masked_embeddings = embeddings * mask
-            summed = torch.sum(masked_embeddings, 1)
-            summed_mask = torch.clamp(mask.sum(1), min=1e-9)
-            mean_pooled = summed / summed_mask
-            
-            # convert from PyTorch tensor to numpy array
-            mean_pooled = mean_pooled.detach().numpy()
+        tokenizer = AutoTokenizer.from_pretrained('microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext')
+        model = AutoModel.from_pretrained('matheusvolpon/WE4LKD_AML_distilbert_1921_{}'.format(loaded_model[-4:]))
 
-            # calculate
-            similarities = flat_list(cosine_similarity([mean_pooled[0]], mean_pooled[1:]).tolist())
-            st.markdown(similarities)
+        # initialize dictionary that will contain tokenized sentences
+        tokens = {'input_ids': [], 'attention_mask': []}
+
+        for sentence in sentences:
+            # tokenize sentence and append to dictionary lists
+            new_tokens = tokenizer.encode_plus(sentence, max_length=128, truncation=True,
+                                               padding='max_length', return_tensors='pt')
+            tokens['input_ids'].append(new_tokens['input_ids'][0])
+            tokens['attention_mask'].append(new_tokens['attention_mask'][0])
+
+        # reformat list of tensors into single tensor
+        tokens['input_ids'] = torch.stack(tokens['input_ids'])
+        tokens['attention_mask'] = torch.stack(tokens['attention_mask'])
+
+        outputs = model(**tokens)
+
+        embeddings = outputs.last_hidden_state
+
+        attention_mask = tokens['attention_mask']
+
+        mask = attention_mask.unsqueeze(-1).expand(embeddings.size()).float()
+
+        masked_embeddings = embeddings * mask
+        summed = torch.sum(masked_embeddings, 1)
+        summed_mask = torch.clamp(mask.sum(1), min=1e-9)
+        mean_pooled = summed / summed_mask
+
+        # convert from PyTorch tensor to numpy array
+        mean_pooled = mean_pooled.detach().numpy()
+
+        # calculate
+        similarities = flat_list(cosine_similarity([mean_pooled[0]], mean_pooled[1:]).tolist())
+        st.markdown(similarities)
         
     st.sidebar.header('GitHub Repository')
     st.sidebar.markdown("[![Foo](https://cdn-icons-png.flaticon.com/32/25/25231.png)](https://github.com/matheusvvb-19/WE4LKD-leukemia_w2v)")
