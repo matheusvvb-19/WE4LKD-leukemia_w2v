8c9b4e622de0a4b8ae6e0f6e05b094a9314cde27
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2023-01-04 11:22:59 -0300

    Update 3_BERT.py

--- pages/3_BERT.py
+++ pages/3_BERT.py
@@ -1,1 +1,28 @@
+# IMPORTS:
+import streamlit as st
 
+# GLOBAL VARIABLES:
+
+# MAIN PROGRAM:
+if __name__ == '__main__':
+    hide_streamlit_style = """
+            <style>           
+            footer {
+                visibility: hidden;
+            }
+            
+            footer:after {
+                content:'Developed by Matheus Vargas Volpon Berto.'; 
+                visibility: visible;
+                display: block;
+                position: relative;
+                padding: 5px;
+                top: 2px;
+                color: black;
+            }
+            </style>
+            """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+    
+    st.sidebar.header('GitHub Repository')
+    st.sidebar.markdown("[![Foo](https://cdn-icons-png.flaticon.com/32/25/25231.png)](https://github.com/matheusvvb-19/WE4LKD-leukemia_w2v)")
