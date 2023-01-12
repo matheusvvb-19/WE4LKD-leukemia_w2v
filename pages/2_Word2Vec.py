73aed882892b18575d78fac72150cbfa40821edd
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2023-01-12 14:05:37 -0300

    Update 2_Word2Vec.py

--- pages/2_Word2Vec.py
+++ pages/2_Word2Vec.py
@@ -198,7 +198,7 @@ def append_list(sim_words, words):
         
     return list_of_words
 
-def display_scatterplot_3D(model, user_input=None, words=None, label=None, color_map=None, annotation='On',  dim_red = 'TSNE', perplexity = 0, learning_rate = 0, iteration = 0, topn=0, sample=10):
+def display_scatterplot_3D(model, user_input=None, words=None, label=None, color_map=None, annotation='On',  dim_red = 'TSNE', perplexity = 5, learning_rate = 0, iteration = 0, topn=0, sample=10):
     if words == None:
         if sample > 0:
             words = np.random.choice(list(model.wv.vocab.keys()), sample)
@@ -326,7 +326,7 @@ def horizontal_bar(word, similarity, input_word=''):
     plot_figure = go.Figure(data = data, layout = layout)
     st.plotly_chart(plot_figure)
 
-def display_scatterplot_2D(model, user_input=None, words=None, label=None, color_map=None, annotation='On', dim_red = 'TSNE', perplexity = 0, learning_rate = 0, iteration = 0, topn=0, sample=10):
+def display_scatterplot_2D(model, user_input=None, words=None, label=None, color_map=None, annotation='On', dim_red = 'TSNE', perplexity = 5, learning_rate = 0, iteration = 0, topn=0, sample=10):
     if words == None:
         if sample > 0:
             words = np.random.choice(list(model.wv.vocab.keys()), sample)
