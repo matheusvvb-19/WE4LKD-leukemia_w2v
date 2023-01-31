1acf0d54ece0dcfcec62e364a8758527ad57b091
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2023-01-31 18:32:05 -0300

    Update 3_BERT.py

--- pages/3_BERT.py
+++ pages/3_BERT.py
@@ -11,8 +11,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 def get_sentences_dataset(year):
     df = pd.read_csv('https://docs.google.com/spreadsheets/d/' + 
                    '1r2pd73pRjl9JNx9DbNYT72xMht-7yUz5xjmA3Xe0lIE' +
-                   '/export?gid=0&format=csv',
-                  )
+                   '/export?gid=0&format=csv', sep=',', escapechar='\\')
     #df = pd.read_csv('sentences_dataset.csv', sep='|')
     df = df[(df["filename"].values <= year)]
     
