77f163cc96e7bf143247d78c32b5dbb4e7cfd4d5
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2023-04-25 22:39:30 -0300

    Create push_to_hub.py

---
+++ bert/push_to_hub.py
@@ -0,0 +1,27 @@
+##################################################
+## Uploads trained Distilbert language models to Hugging Face Models Hub.
+##################################################
+## Author: {name}
+## Copyright: Copyright 2022, Discovering Latent Knowledge in medical paper on Acute Myeloid Leukemia
+## Email: {contact_email}
+## Based on: https://huggingface.co/docs/transformers/model_sharing#use-the-pushtohub-function
+##################################################
+
+import os
+from transformers import DistilBertForMaskedLM
+from transformers import AutoTokenizer
+
+if __name__ == '__main__':
+    # CONSTANT(S):
+    MODELS = sorted([f.path for f in os.scandir('./distilbert/') if f.is_dir()])
+    
+    # variable(s):
+    model_name_template = 'matheusvolpon/WE4LKD_AML_distilbert_1921_'
+
+    for m in MODELS:
+        model = DistilBertForMaskedLM.from_pretrained(m, local_files_only=True)
+        tokenizer = AutoTokenizer.from_pretrained(m, local_files_only=True)
+
+        print('Pushing model {} to hub\n'.format(m))
+        model.push_to_hub(model_name_template + m[-4:])
+        tokenizer.push_to_hub(model_name_template + m[-4:])
