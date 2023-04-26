6f7baaf9601458cf142defed8e4fe983645bcd09
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2023-04-26 00:00:04 -0300

    Update clean_synonyms.py

--- pubchem/clean_synonyms.py
+++ pubchem/clean_synonyms.py
@@ -1,6 +1,16 @@
+##################################################
+## Preprocess the synonyms table (PubChem).
+##################################################
+## Author: {name}
+## Copyright: Copyright 2022, Discovering Latent Knowledge in medical paper on Acute Myeloid Leukemia
+## Email: {contact_email}
+##################################################
+
+# IMPORTS:
 from pubchem.clean_summaries import read_csv_table_files, words_preprocessing, summary_column_preprocessing, to_csv
 import pyspark.sql.functions as F
 
+# MAIN PROGRAM:
 if __name__ == '__main__':
     synonyms = read_csv_table_files('/home/matheus/synonyms_table.csv')\
                 .withColumn('synonym', summary_column_preprocessing(F.col('synonym')))
