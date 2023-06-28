9d216cd4e7156c50843e1c088adfcd553e6b27a1
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2023-06-28 20:25:16 -0300

    Update titles_and_synonyms.py

--- pubchem/titles_and_synonyms.py
+++ pubchem/titles_and_synonyms.py
@@ -1,9 +1,9 @@
 ##################################################
 ## Generates a csv file for the CID-Titles and CID-Synonyms DataFrames.
 ##################################################
-## Author: {name}
+## Author: Matheus Vargas Volpon Berto
 ## Copyright: Copyright 2022, Discovering Latent Knowledge in medical paper on Acute Myeloid Leukemia
-## Email: {contact_email}
+## Email: matheusvvb@hotmail.com
 ##################################################
 
 # IMPORTS:
