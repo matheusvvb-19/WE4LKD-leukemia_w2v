fbb5faa9cdbfc64b685606edcaf13b9c327b8623
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2023-06-28 20:27:22 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,9 +1,9 @@
 ##################################################
 ## Displays the Home page of the developed Streamlit web application.
 ##################################################
-## Author: {name}
+## Author: Matheus Vargas Volpon Berto
 ## Copyright: Copyright 2022, Discovering Latent Knowledge in medical paper on Acute Myeloid Leukemia
-## Email: {contact_email}
+## Email: matheusvvb@hotmail.com
 ##################################################
 
 # IMPORTS:
