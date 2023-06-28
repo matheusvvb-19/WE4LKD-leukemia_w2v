a8f45971c13a6a16786e159cb5eeb16c61fae8ff
matheusvvb-19 <84142397+matheusvvb-19@users.noreply.github.com>
2023-06-28 20:26:40 -0300

    Update train_yoy.py

--- word2vec/train_yoy.py
+++ word2vec/train_yoy.py
@@ -1,9 +1,9 @@
 ##################################################
 ## Trains incremental Word2Vec or FastText language models from the selected hyperparameter combination.
 ##################################################
-## Author: {name}
+## Author: Matheus Vargas Volpon Berto
 ## Copyright: Copyright 2022, Discovering Latent Knowledge in medical paper on Acute Myeloid Leukemia
-## Email: {contact_email}
+## Email: matheusvvb@hotmail.com
 ##################################################
 
 # IMPORTS:
